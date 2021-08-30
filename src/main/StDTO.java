f45d5ac9f0329f2d77510baa07894c5f516cbe9f
parksunyoung <iss002@naver.com>
2021-08-30 18:10:25 +0900

    project04 create.

---
+++ src/main/StDTO.java
@@ -0,0 +1,18 @@
+package main;
+
+public class StDTO {
+	private String stNum;
+	private String name;
+	public String getStNum() {
+		return stNum;
+	}
+	public void setStNum(String stNum) {
+		this.stNum = stNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
